c46de5fbcba25b9bf4775bad35195043decb66d1
faiza-khatri <161750227+faiza-khatri@users.noreply.github.com>
2024-08-09 12:47:48 +0500

    added all project files

--- src/configuration.jsx
+++ src/configuration.jsx
@@ -8,14 +8,14 @@ import { getAuth } from "firebase/auth";  // Import for authentication
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: "pantry-tracker-9a381.firebaseapp.com",
+  apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`,
   databaseURL: "https://pantry-tracker-9a381-default-rtdb.firebaseio.com",
-  projectId: "pantry-tracker-9a381",
-  storageBucket: "pantry-tracker-9a381.appspot.com",
-  messagingSenderId: "682112584787",
-  appId: "1:682112584787:web:0e28a319d689a6319ecaa6",
-  measurementId: "G-EYQB27C7QT"
+  projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_FIREBASE_APP_ID}`,
+  measurementId: `${process.env.REACT_APP_FIREBASE_MEASUREMENT_ID}`,
 };
 
 // Initialize Firebase
